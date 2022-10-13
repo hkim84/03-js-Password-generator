79da0f33153f730204e103b35c5ce6401fdacacc
hkim84 <112454703+hkim84@users.noreply.github.com>
2022-10-12 21:45:06 -0700

    added some script number length and variables

--- script.js
+++ script.js
@@ -13,17 +13,33 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 
-// Adding variable of Character to use in random password
-var {
-  upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  lowerCase= "abcdefghijklmnopqrstuvwxyz";
-  number= "0123456789";
-  symbol= "!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
-}
+
+
+
+// a function to generate password
 
 function generatePassword() {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
   var Password= "";
+//1. Adding variable of Character to use in random password
+//2. Password length from 8 to 128 characters.
+//3. Send out an alert if not choosen.
+//4.setting choice to use upper character, lower character,number, and symbols.
+//5.generate password.
+
+//Global var
+
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numericvalue = "0123456789";
+  var specialsymbol = "!@#$%^&*()-+=~?<>";
+
+//Password length choice from 8 to 128 characters
+  var pslength = prompt("Choose a number between 8 through 128 for you password length");
+  if (pslength >=7){
+    if (pslength >=8 && pslength <= 128){
+      
+    }
+  }
 
 }
-
